b9d053ac2ef48f3c5ac7af4467fbe2cc2cbbf0c8
mohammad <mohammsep@gmail.com>
2017-06-25 23:30:03 +1000

    Eslint fixes. updating gitignore.

--- src/actions/todoActions.js
+++ src/actions/todoActions.js
@@ -1,19 +1,18 @@
-import * as types from './actionTypes';
-import todosApi from '../api/mockTodosApi';
-import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
+import * as types from './actionTypes'
+import todosApi from '../api/mockTodosApi'
+import { beginAjaxCall } from './ajaxStatusActions'
 
-export function loadTodosSuccess(todos) {
-  return { type: types.LOAD_TODOS_SUCCESS, todos};
+export function loadTodosSuccess (todos) {
+  return { type: types.LOAD_TODOS_SUCCESS, todos }
 }
 
-
-export function loadTodos() {
-  return function(dispatch) {
-    dispatch(beginAjaxCall());
+export function loadTodos () {
+  return function (dispatch) {
+    dispatch(beginAjaxCall())
     return todosApi.getAllTodos().then(todos => {
-      dispatch(loadTodosSuccess(todos));
+      dispatch(loadTodosSuccess(todos))
     }).catch(error => {
-      throw(error);
-    });
-  };
+      throw (error)
+    })
+  }
 }
